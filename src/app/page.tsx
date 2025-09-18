246e1d5a60be7b0e71d3fad5ea4c6962f74c3a79
kaidyer <kaidyer29@gmail.com>
2025-09-17 20:58:12 -0400

    centered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,28 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [teamOne, setT1Name] = useState("Enter Your Team Name");
-  const [teamTwo, setT2Name] = useState("Enter Your Team Name");
+  const [teamOne, setT1Name] = useState("Team Kai");
+  const [teamTwo, setT2Name] = useState("Team Jenna");
 
   return (
-    <div className="p-8">
-      <div className="flex gap-4 mb-6">
+    <div className="p-8 flex content-center">
+      <div className="w-1/2 gap-4 mb-6">
         <input
           type="text"
           value={teamOne}
           onChange={(e) => setT1Name(e.target.value)}
-          className="text-4xl font-bold border-b border-gray-400 mb-6 w-full focus:outline-none"
+          className="text-4xl font-bold border-b border-gray-400 mb-6 w-full text-center focus:outline-none"
         />
+      </div>
+
+      <div className="w-px bg-gray-400 h-screen"></div> {/* vertical line */}
+
+      <div className="w-1/2 gap-4 mb-6">
         <input
           type="text"
           value={teamTwo}
           onChange={(e) => setT2Name(e.target.value)}
-          className="text-4xl font-bold border-b border-gray-400 mb-6 w-full focus:outline-none"
+          className="text-4xl font-bold border-b border-gray-400 mb-6 w-full text-center focus:outline-none"
         />
       </div>
     </div>
